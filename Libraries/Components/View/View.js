bf19ffaa04148bedc80b5c110973fc46f0e68198
Rajiv Shah <rajivshah1@icloud.com>
2018-10-25 12:42:24 -0400

    Fix displayName prop on View and Text components

--- Libraries/Components/View/View.js
+++ Libraries/Components/View/View.js
@@ -48,6 +48,7 @@ if (__DEV__) {
   };
   // $FlowFixMe - TODO T29156721 `React.forwardRef` is not defined in Flow, yet.
   ViewToExport = React.forwardRef(View);
+  ViewToExport.displayName = 'View';
 }
 
 module.exports = ((ViewToExport: $FlowFixMe): typeof ViewNativeComponent);
